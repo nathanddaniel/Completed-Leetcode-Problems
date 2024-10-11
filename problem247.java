ca365ee85ef481d6709b11db80bfe29206288d7d
Nathan Daniel <20nmd1@queensu.ca>
2024-10-11 15:10:21 -0400

    Completed the solution for Leetcode Problem 247

---
+++ problem247.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class problem247 {
+
+    public boolean isAnagram(String s, String t) {
+
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+}
